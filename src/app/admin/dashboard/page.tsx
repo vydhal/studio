65ed93b22ea9817d3c853f440e7aedd6dabd5a7c
Vydhal Silva <vydhal@gmail.com>
2025-07-14 18:49:24 +0000

    ainda está!!!!!

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -4,9 +4,7 @@ export default function DashboardPage() {
     // O fetching dos dados agora é feito de forma assíncrona no servidor 
     // e passado para o cliente, usando Firebase.
     return (
-        <div className="space-y-4">
-            <DashboardClient />
-        </div>
+        <DashboardClient />
     );
 }
 
