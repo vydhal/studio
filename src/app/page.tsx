ea9fdac563bbf2d119c7695d5f1f1a9af521fb00
Vydhal Silva <vydhal@gmail.com>
2025-10-09 16:06:31 +0000

    ainda assim ano corrigiu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 container">
         <HomeContent />
       </main>
       <Footer />
