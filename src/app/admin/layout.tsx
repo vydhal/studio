fab1acb6debd2c97aab4f896881b77168615cdda
Cayque Silva <cayque.ti@gmail.com>
2025-10-29 17:11:36 +0000

    ajuste minhas paginas no painel admin.
    Elas estão com um espaço enorem

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -5,14 +5,12 @@ export default function AdminLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // In a real application, you would protect this layout with authentication.
-  // For this test environment, we are allowing access directly.
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
-       <AdminSidebar />
-        <main className="flex flex-col bg-muted/40 lg:pl-72">
-            {children}
-        </main>
+    <div className="flex min-h-screen w-full bg-muted/40">
+      <AdminSidebar />
+      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+        {children}
+      </main>
     </div>
   );
 }
