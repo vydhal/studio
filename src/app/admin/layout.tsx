2d01fd450b436762c2a4cb5b54b16a87c4226b25
Cayque Silva <cayque.ti@gmail.com>
2025-10-29 17:28:00 +0000

    aplique a correção

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -8,11 +8,9 @@ export default function AdminLayout({
   return (
     <div className="flex min-h-screen w-full bg-muted/40">
       <AdminSidebar />
-      <div className="flex flex-col flex-1">
-        <main className="flex-1 p-4 md:p-8">
-          {children}
-        </main>
-      </div>
+      <main className="flex-1 p-4 md:p-8">
+        {children}
+      </main>
     </div>
   );
 }
